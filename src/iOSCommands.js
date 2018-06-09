3e494c0a67bcf26416c940d347153a6cb1b9e3f7
vishnubkk <vishnuvarma@ddproperty.com>
2018-06-09 13:39:17 +0700

    First commit

---
+++ src/iOSCommands.js
@@ -0,0 +1,10 @@
+const cmd = require("node-cmd");
+
+console.log("First");
+async function getBundleId() {
+    // cmd.run('osascript -e \'id of app " + appPath + "\'');
+    console.log("First");
+    
+    // await cmd.run('node -v')
+}
+module.exports = getBundleId;
